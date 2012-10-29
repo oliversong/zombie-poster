1a97cac6f5b9e00faddc2c8c7afcef745d66bf50
oliversong <oliversong@tumblr.com>
2012-10-28 17:24:54 -0700

    Switching to casper.js siiiigh zombie why do you fail me every time

--- submit.js
+++ submit.js
@@ -1,24 +1,16 @@
-var Browser = require("zombie");
-var assert = require("assert");
+var casper = require('casper').create();
 
 //submit to hackernews
 function submit(url, title){
     console.log("Submitting");
-    browser = new Browser();
-    browser.visit("http://news.ycombinator.com/newslogin?whence=news",function(){
-        browser.
-            fill("u","oliversong").
-            fill("p","uchiha").
-            pressButton("login", function(){
-                console.log(browser.cookies().all());
-                browser.visit("http://news.ycombinator.com/"+"submitlink?u="+encodeURIComponent(url)+"&t="+encodeURIComponent(title), function () {
-                    console.log(browser.html());
-                    browser.pressButton("submit", function() {
-                        assert.ok(browser.success);
-                    })
-                });
-            });
+    phantom.addCookie({"name":"user","value":"FwVp29RY","domain":"news.ycombinator.com"});
+    casper.start("http://news.ycombinator.com/submitlink?u="+encodeURIComponent(url)+"&t="+encodeURIComponent(title),function(){
+        this.click('form table tbody:nth-child(3) input');
     });
+    casper.then(function(){
+        console.log("clicked submit, new location is" + this.getCurrentUrl());
+    });
+    casper.run();
 }
 
 submit("http://www.technologyreview.com/news/506127/connectome/","DNA Sequencing Could Map the Brain's Wiring");
